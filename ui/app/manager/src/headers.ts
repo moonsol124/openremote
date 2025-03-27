cadd0938b710ffaf378e8758e2628d3fee8f4115
moonsol124 <moonsol124@gmail.com>
2025-03-27 08:35:17 +0100

    f

--- ui/app/manager/src/headers.ts
+++ ui/app/manager/src/headers.ts
@@ -7,7 +7,7 @@ export function headerItemMap<S extends AppStateKeyed, A extends AnyAction>(orAp
     return {
         icon: "map",
         href: getMapRoute(),
-        text: "mapasd...."
+        text: "mapasd... another test."
     };
 }
 
